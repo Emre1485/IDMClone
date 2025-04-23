017825d869e9be5b1f33bccc07f3e6cf5f6dc197
emre alpay <emrebalp61@gmail.com>
2025-04-23 22:55:59 +0300

    update

--- IDMClone/Form1.Designer.cs
+++ IDMClone/Form1.Designer.cs
@@ -1,115 +1,152 @@
-﻿namespace IDMClone
+﻿namespace IDMClone;
+
+partial class Form1
 {
-    partial class Form1
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            txtUrl = new TextBox();
-            btnDownload = new Button();
-            lblStatus = new Label();
-            progressBar1 = new ProgressBar();
-            btnAdvancedDownload = new Button();
-            panelProgressBars = new Panel();
-            SuspendLayout();
-            // 
-            // txtUrl
-            // 
-            txtUrl.Location = new Point(31, 38);
-            txtUrl.Name = "txtUrl";
-            txtUrl.Size = new Size(317, 27);
-            txtUrl.TabIndex = 0;
-            // 
-            // btnDownload
-            // 
-            btnDownload.Location = new Point(354, 36);
-            btnDownload.Name = "btnDownload";
-            btnDownload.Size = new Size(157, 29);
-            btnDownload.TabIndex = 1;
-            btnDownload.Text = "Download";
-            btnDownload.UseVisualStyleBackColor = true;
-            btnDownload.Click += btnDownload_Click;
-            // 
-            // lblStatus
-            // 
-            lblStatus.AutoSize = true;
-            lblStatus.Location = new Point(31, 123);
-            lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(49, 20);
-            lblStatus.TabIndex = 2;
-            lblStatus.Text = "Status";
-            // 
-            // progressBar1
-            // 
-            progressBar1.Location = new Point(31, 71);
-            progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(317, 29);
-            progressBar1.TabIndex = 3;
-            // 
-            // btnAdvancedDownload
-            // 
-            btnAdvancedDownload.Location = new Point(354, 71);
-            btnAdvancedDownload.Name = "btnAdvancedDownload";
-            btnAdvancedDownload.Size = new Size(157, 29);
-            btnAdvancedDownload.TabIndex = 4;
-            btnAdvancedDownload.Text = "AdvancedDownload";
-            btnAdvancedDownload.UseVisualStyleBackColor = true;
-            btnAdvancedDownload.Click += btnAdvancedDownload_ClickAsync;
-            // 
-            // panelProgressBars
-            // 
-            panelProgressBars.Location = new Point(31, 163);
-            panelProgressBars.Name = "panelProgressBars";
-            panelProgressBars.Size = new Size(480, 382);
-            panelProgressBars.TabIndex = 5;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(552, 559);
-            Controls.Add(panelProgressBars);
-            Controls.Add(btnAdvancedDownload);
-            Controls.Add(progressBar1);
-            Controls.Add(lblStatus);
-            Controls.Add(btnDownload);
-            Controls.Add(txtUrl);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
-            PerformLayout();
-        }
-
-        #endregion
-
-        private TextBox txtUrl;
-        private Button btnDownload;
-        private Label lblStatus;
-        private ProgressBar progressBar1;
-        private Button btnAdvancedDownload;
-        private Panel panelProgressBars;
+        if (disposing && (components != null))
+            components.Dispose();
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    private void InitializeComponent()
+    {
+        txtUrl = new TextBox();
+        btnDownload = new Button();
+        btnAdvancedDownload = new Button();
+        lblFileNameTitle = new Label();
+        lblFileNameValue = new Label();
+        lblFileSizeTitle = new Label();
+        lblFileSizeValue = new Label();
+        lblDownloadStatusTitle = new Label();
+        lblStatus = new Label();
+        progressBar1 = new ProgressBar();
+        panelProgressBars = new Panel();
+        SuspendLayout();
+        // 
+        // txtUrl
+        // 
+        txtUrl.Location = new Point(20, 20);
+        txtUrl.Name = "txtUrl";
+        txtUrl.PlaceholderText = "Dosya URL'si girin...";
+        txtUrl.Size = new Size(400, 27);
+        txtUrl.TabIndex = 0;
+        // 
+        // btnDownload
+        // 
+        btnDownload.Location = new Point(430, 20);
+        btnDownload.Name = "btnDownload";
+        btnDownload.Size = new Size(120, 27);
+        btnDownload.TabIndex = 1;
+        btnDownload.Text = "Download";
+        btnDownload.Click += btnDownload_Click;
+        // 
+        // btnAdvancedDownload
+        // 
+        btnAdvancedDownload.Location = new Point(560, 20);
+        btnAdvancedDownload.Name = "btnAdvancedDownload";
+        btnAdvancedDownload.Size = new Size(160, 27);
+        btnAdvancedDownload.TabIndex = 2;
+        btnAdvancedDownload.Text = "Advanced Download";
+        btnAdvancedDownload.Click += btnAdvancedDownload_Click;
+        // 
+        // lblFileNameTitle
+        // 
+        lblFileNameTitle.Location = new Point(20, 60);
+        lblFileNameTitle.Name = "lblFileNameTitle";
+        lblFileNameTitle.Size = new Size(100, 23);
+        lblFileNameTitle.TabIndex = 3;
+        lblFileNameTitle.Text = "Dosya Adı:";
+        // 
+        // lblFileNameValue
+        // 
+        lblFileNameValue.Location = new Point(120, 60);
+        lblFileNameValue.Name = "lblFileNameValue";
+        lblFileNameValue.Size = new Size(600, 20);
+        lblFileNameValue.TabIndex = 4;
+        // 
+        // lblFileSizeTitle
+        // 
+        lblFileSizeTitle.Location = new Point(20, 90);
+        lblFileSizeTitle.Name = "lblFileSizeTitle";
+        lblFileSizeTitle.Size = new Size(100, 23);
+        lblFileSizeTitle.TabIndex = 5;
+        lblFileSizeTitle.Text = "Dosya Boyutu:";
+        // 
+        // lblFileSizeValue
+        // 
+        lblFileSizeValue.Location = new Point(120, 90);
+        lblFileSizeValue.Name = "lblFileSizeValue";
+        lblFileSizeValue.Size = new Size(600, 20);
+        lblFileSizeValue.TabIndex = 6;
+        // 
+        // lblDownloadStatusTitle
+        // 
+        lblDownloadStatusTitle.Location = new Point(20, 120);
+        lblDownloadStatusTitle.Name = "lblDownloadStatusTitle";
+        lblDownloadStatusTitle.Size = new Size(100, 23);
+        lblDownloadStatusTitle.TabIndex = 7;
+        lblDownloadStatusTitle.Text = "Durum:";
+        // 
+        // lblStatus
+        // 
+        lblStatus.Location = new Point(120, 120);
+        lblStatus.Name = "lblStatus";
+        lblStatus.Size = new Size(600, 20);
+        lblStatus.TabIndex = 8;
+        // 
+        // progressBar1
+        // 
+        progressBar1.Location = new Point(20, 160);
+        progressBar1.Name = "progressBar1";
+        progressBar1.Size = new Size(700, 25);
+        progressBar1.TabIndex = 9;
+        // 
+        // panelProgressBars
+        // 
+        panelProgressBars.AutoScroll = true;
+        panelProgressBars.Location = new Point(20, 200);
+        panelProgressBars.Name = "panelProgressBars";
+        panelProgressBars.Size = new Size(700, 152);
+        panelProgressBars.TabIndex = 10;
+        // 
+        // Form1
+        // 
+        AutoScaleMode = AutoScaleMode.None;
+        ClientSize = new Size(750, 372);
+        Controls.Add(txtUrl);
+        Controls.Add(btnDownload);
+        Controls.Add(btnAdvancedDownload);
+        Controls.Add(lblFileNameTitle);
+        Controls.Add(lblFileNameValue);
+        Controls.Add(lblFileSizeTitle);
+        Controls.Add(lblFileSizeValue);
+        Controls.Add(lblDownloadStatusTitle);
+        Controls.Add(lblStatus);
+        Controls.Add(progressBar1);
+        Controls.Add(panelProgressBars);
+        Name = "Form1";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "IDM Clone - Dosya İndirme";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private TextBox txtUrl;
+    private Button btnDownload;
+    private Button btnAdvancedDownload;
+    private Label lblFileNameTitle;
+    private Label lblFileNameValue;
+    private Label lblFileSizeTitle;
+    private Label lblFileSizeValue;
+    private Label lblDownloadStatusTitle;
+    private Label lblStatus;
+    private ProgressBar progressBar1;
+    private Panel panelProgressBars;
 }
--- IDMClone/Form1.cs
+++ IDMClone/Form1.cs
@@ -1,192 +1,250 @@
 using System.ComponentModel;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace IDMClone
 {
     public partial class Form1 : Form
     {
-        private WebClient webClient;
-        private readonly HttpClient httpClient = new HttpClient();
+        private HttpClient httpClient = new HttpClient();
+        private int maxThreads = 4;
+        private string fileName = "";
 
         public Form1()
         {
             InitializeComponent();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
         }
 
-        private void btnDownload_Click(object sender, EventArgs e)
+        private async void btnDownload_Click(object sender, EventArgs e)
         {
-            string url = txtUrl.Text.Trim();
-            if (string.IsNullOrEmpty(url))
-            {
-                MessageBox.Show("Lütfen dosya url adresini giriniz!");
-                return;
-            }
-
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.FileName = System.IO.Path.GetFileName(url);
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                webClient = new WebClient();
-                webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
-                webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
-
-                try
-                {
-                    Uri uri = new Uri(url);
-                    webClient.DownloadFileAsync(uri, saveFileDialog.FileName);
-                    lblStatus.Text = "Download started...";
-                }
-                catch (Exception ex)
-                {
-                    lblStatus.Text = "Error: " + ex.Message;
-                }
-            }
-        }
-
-        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            progressBar1.Value = e.ProgressPercentage;
-            lblStatus.Text = $"Downloading...%{e.ProgressPercentage}";
-        }
-
-        private void WebClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            if (e.Cancelled)
-                lblStatus.Text = "Download cancelled.";
-            else if (e.Error != null)
-                lblStatus.Text = "An error ocurred: " + e.Error.Message;
-            else
-                lblStatus.Text = "Download completed!";
-        }
-
-        private async void btnAdvancedDownload_ClickAsync(object sender, EventArgs e)
-        {
-            string url = txtUrl.Text.Trim();
-            if (string.IsNullOrWhiteSpace(url))
-            {
-                MessageBox.Show("Lütfen bir URL girin.");
-                return;
-            }
+            string url = txtUrl.Text;
+            if (string.IsNullOrWhiteSpace(url)) return;
 
             try
             {
+                lblStatus.Text = "Baðlantý kuruluyor...";
+                HttpResponseMessage response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                response.EnsureSuccessStatusCode();
 
-                string outputDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+                string suggestedFileName = GetFileNameFromUrlOrHeaders(url, response);
 
-                if (!Directory.Exists(outputDirectory))
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.FileName = suggestedFileName;
+
+                if (saveDialog.ShowDialog() != DialogResult.OK)
                 {
-                    Directory.CreateDirectory(outputDirectory);  // Klasör yoksa oluþtur
+                    lblStatus.Text = "Ýndirme iptal edildi.";
+                    return;
                 }
 
-                string fileName = Path.GetFileName(url);
-                string validFileName = string.Concat(fileName.Split(Path.GetInvalidFileNameChars()));
-                string outputPath = Path.Combine(outputDirectory, validFileName);
+                fileName = saveDialog.FileName;
+                lblFileNameValue.Text = Path.GetFileName(fileName);
+                lblFileSizeValue.Text = FormatBytes(response.Content.Headers.ContentLength ?? 0);
 
-                long totalSize = await GetFileSizeAsync(url);
-                lblStatus.Text = $"Total size: {totalSize} bytes";
+                lblStatus.Text = "Ýndiriliyor...";
+                var stream = await response.Content.ReadAsStreamAsync();
 
-                using (var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
                 {
-                    fs.SetLength(totalSize);
-                }
+                    byte[] buffer = new byte[8192];
+                    long totalRead = 0;
+                    int read;
+                    long totalSize = response.Content.Headers.ContentLength ?? 1;
 
-
-                // 2. 4 parçaya böl
-                int numParts = 4;
-                long partSize = totalSize / numParts;
-                List<Task> tasks = new List<Task>();
-
-                CreateProgressBars(numParts); // önce progress barlarý oluþtur
-
-                for (int i = 0; i < numParts; i++)
-                {
-                    long start = i * partSize;
-                    long end = (i == numParts - 1) ? totalSize - 1 : (start + partSize - 1);
-
-                    int index = i;
-                    var progress = new Progress<int>(value =>
+                    while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
                     {
-                        progressBars[index].Value = value;
-                    });
+                        await fileStream.WriteAsync(buffer, 0, read);
+                        totalRead += read;
 
-                    tasks.Add(DownloadPart(url, outputPath, start, end, progress));
+                        int percent = (int)((totalRead * 100) / totalSize);
+                        progressBar1.Value = Math.Min(percent, 100);
+                        lblStatus.Text = $"Ýndiriliyor... %{percent}";
+                    }
                 }
 
-
-                // 3. Tüm parçalar indirilsin
-                await Task.WhenAll(tasks);
-
-                lblStatus.Text = "Download comlpeted!";
+                if (File.Exists(fileName))
+                {
+                    lblStatus.Text = $"Ýndirme tamamlandý: {fileName}";
+                }
+                else
+                {
+                    lblStatus.Text = "Dosya oluþturulamadý.";
+                }
             }
             catch (Exception ex)
             {
-                lblStatus.Text = "Error: " + ex.Message;
+                lblStatus.Text = "Hata: " + ex.Message;
             }
         }
 
-        private async Task<long> GetFileSizeAsync(string url)
+        private async void btnAdvancedDownload_Click(object sender, EventArgs e)
         {
-            using var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+            string url = txtUrl.Text;
+            if (string.IsNullOrWhiteSpace(url)) return;
 
-            if (!response.IsSuccessStatusCode)
-                throw new Exception("HEAD request failed.");
-
-            // Bazý sunucular Accept-Ranges göndermez, ama yine de Range destekler
-            var acceptRanges = response.Headers.AcceptRanges;
-            if (acceptRanges == null || !acceptRanges.Contains("bytes"))
+            try
             {
-                throw new Exception("The server does not support partial downloads.");
-            }
+                lblStatus.Text = "Baðlantý kuruluyor...";
 
-            return response.Content.Headers.ContentLength ?? throw new Exception("Could not retrive file size.");
-        }
+                // HEAD isteði yerine GET ile Range kullanarak sunucu desteðini kontrol et
+                var testRequest = new HttpRequestMessage(HttpMethod.Get, url);
+                testRequest.Headers.Range = new RangeHeaderValue(0, 0);
+                var testResponse = await httpClient.SendAsync(testRequest);
 
-
-        public async Task DownloadPart(string url, string outputPath, long start, long end, IProgress<int> progress)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Range = new System.Net.Http.Headers.RangeHeaderValue(start, end);
-
-            using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-            response.EnsureSuccessStatusCode();
-
-            using var stream = await response.Content.ReadAsStreamAsync();
-            using var outputStream = new FileStream(outputPath, FileMode.Open, FileAccess.Write, FileShare.Write);
-
-            outputStream.Seek(start, SeekOrigin.Begin);
-
-            byte[] buffer = new byte[8192];
-            long totalRead = 0;
-            int read;
-            while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-            {
-                await outputStream.WriteAsync(buffer, 0, read);
-                totalRead += read;
-
-                int percent = (int)((totalRead * 100) / (end - start + 1));
-                progress.Report(percent);
-            }
-        }
-
-
-        List<ProgressBar> progressBars = new List<ProgressBar>();
-        private void CreateProgressBars(int count)
-        {
-            panelProgressBars.Controls.Clear();
-            progressBars.Clear();
-
-            for (int i = 0; i < count; i++)
-            {
-                ProgressBar pb = new ProgressBar
+                // Range desteði kontrolü
+                if (testResponse.StatusCode != HttpStatusCode.PartialContent)
                 {
-                    Width = panelProgressBars.Width - 20,
-                    Height = 20,
-                    Top = i * 25,
-                    Left = 10,
-                    Maximum = 100
-                };
-                panelProgressBars.Controls.Add(pb);
-                progressBars.Add(pb);
+                    lblStatus.Text = "Sunucu parçalý indirmeyi desteklemiyor!";
+                    return;
+                }
+
+                long totalSize = testResponse.Content.Headers.ContentRange?.Length ?? 0;
+                if (totalSize <= 0)
+                {
+                    lblStatus.Text = "Geçersiz dosya boyutu!";
+                    return;
+                }
+
+                // Dosya adýný belirle
+                string suggestedFileName = GetFileNameFromUrlOrHeaders(url, testResponse);
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.FileName = suggestedFileName;
+
+                if (saveDialog.ShowDialog() != DialogResult.OK)
+                {
+                    lblStatus.Text = "Ýndirme iptal edildi.";
+                    return;
+                }
+                fileName = saveDialog.FileName;
+
+                lblStatus.Text = "Parçalý indirme baþlatýlýyor...";
+                string tempFile = fileName + ".tmp";
+
+                // Geçici dosya
+                using (File.Create(tempFile)) { }
+
+                // Progress bar'larý 
+                panelProgressBars.Controls.Clear();
+                ProgressBar[] partBars = new ProgressBar[maxThreads];
+                for (int i = 0; i < maxThreads; i++)
+                {
+                    partBars[i] = new ProgressBar
+                    {
+                        Location = new Point(0, i * 30),
+                        Size = new Size(700, 25),
+                        Minimum = 0,
+                        Maximum = 100
+                    };
+                    panelProgressBars.Controls.Add(partBars[i]);
+                }
+
+               
+                long partSize = totalSize / maxThreads;
+                List<Task> tasks = new List<Task>();
+
+                for (int i = 0; i < maxThreads; i++)
+                {
+                    int partNumber = i;
+                    long start = partNumber * partSize;
+                    long end = (partNumber == maxThreads - 1) ? totalSize - 1 : start + partSize - 1;
+
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        using (var client = new HttpClient())
+                        {
+                            client.Timeout = Timeout.InfiniteTimeSpan;
+                            var request = new HttpRequestMessage(HttpMethod.Get, url);
+                            request.Headers.Range = new RangeHeaderValue(start, end);
+
+                            var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                            response.EnsureSuccessStatusCode();
+
+                            using (var stream = await response.Content.ReadAsStreamAsync())
+                            using (var fileStream = new FileStream(tempFile, FileMode.Open, FileAccess.Write, FileShare.Write))
+                            {
+                                fileStream.Seek(start, SeekOrigin.Begin);
+                                byte[] buffer = new byte[81920];
+                                int bytesRead;
+                                long totalRead = 0;
+
+                                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    await fileStream.WriteAsync(buffer, 0, bytesRead);
+                                    totalRead += bytesRead;
+
+                                    // Progress güncelleme
+                                    int progress = (int)((totalRead * 100) / (end - start + 1));
+                                    Invoke(new Action(() => partBars[partNumber].Value = progress));
+                                }
+                            }
+                        }
+                    }));
+                }
+
+                await Task.WhenAll(tasks);
+                File.Move(tempFile, fileName, true);
+                lblStatus.Text = "Ýndirme baþarýyla tamamlandý!";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"Hata: {ex.Message}";
+                if (File.Exists(fileName + ".tmp"))
+                    File.Delete(fileName + ".tmp");
+            }
+        }
+
+        private string GetFileNameFromUrlOrHeaders(string url, HttpResponseMessage response)
+        {
+            var contentDisposition = response.Content.Headers.ContentDisposition;
+            if (contentDisposition != null && !string.IsNullOrEmpty(contentDisposition.FileName))
+                return contentDisposition.FileName.Trim('"');
+
+            return Path.GetFileName(new Uri(url).LocalPath);
+        }
+
+        private string FormatBytes(long bytes)
+        {
+            if (bytes >= 1_000_000_000) return (bytes / 1_000_000_000.0).ToString("0.00") + " GB";
+            if (bytes >= 1_000_000) return (bytes / 1_000_000.0).ToString("0.00") + " MB";
+            if (bytes >= 1_000) return (bytes / 1_000.0).ToString("0.00") + " KB";
+            return bytes + " B";
+        }
+
+        private string GetFileExtensionFromContentType(string contentType)
+        {
+            if (string.IsNullOrEmpty(contentType)) return ".bin"; 
+
+            switch (contentType.ToLower())
+            {
+                case "application/pdf":
+                    return ".pdf";
+                case "image/jpeg":
+                    return ".jpg";
+                case "image/png":
+                    return ".png";
+                case "text/plain":
+                    return ".txt";
+                case "application/zip":
+                    return ".zip";
+                case "application/octet-stream":
+                    return ".bin";
+                default:
+                    return ".bin"; 
+            }
+        }
+
+        private void SaveFile(string filePath)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.FileName = fileName; 
+            saveDialog.Filter = "All Files|*.*";
+            saveDialog.DefaultExt = Path.GetExtension(fileName);
+            saveDialog.AddExtension = true;
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.Copy(filePath, saveDialog.FileName, true);
             }
         }
 
